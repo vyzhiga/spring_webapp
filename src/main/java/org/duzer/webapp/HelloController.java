46bd57d9265d2e6446b818bc2dc2fc93f086c4dd
vyzhiga <vyzhiga@gmail.com>
2017-02-03 11:08:23 +0300

    Add to git some forgotten files

---
+++ src/main/java/org/duzer/webapp/HelloController.java
@@ -0,0 +1,24 @@
+package org.duzer.webapp;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.ModelMap;
+
+@Controller
+public class HelloController{
+
+    private final static Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    @RequestMapping(value="/", method = RequestMethod.GET)
+    public String printHello(ModelMap model) {
+
+        logger.debug("Returned Hello string");
+
+        return "index";
+
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <meta http-equiv="refresh" content="5; URL=books.jsp">
+    </head>
+    <body>
+        <h2>Redirecting</h2>
+    </body>
+</html>
